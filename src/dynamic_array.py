b082f96561a0b64a93ee9019427b899470b6a02e
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-09 12:57:06 -0400

    start insert method, not quite finished yet

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -3,3 +3,20 @@ class DynamicArray:
         self.capacity = capacity
         self.count = 0
         self.storage = [None] * self.capacity
+
+    def insert(self, index, value):
+
+        # make sure we open space
+        if self.count >= self.capacity:
+            # TODO: Make array dynamically resize
+            print("ERROR: Array is full!")
+            return
+
+        # make sure index is in range
+        if index > self.count:
+            print("Error: Index out of range")
+        # shift everything over to right
+
+        # insert our value
+        self.storage[index] = value
+        self.count += 1
