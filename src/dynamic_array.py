f982d0422dda9cec6ec69bd5478f0bb5e576bfe9
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-09 12:50:32 -0400

    add dynamic array file and def init method

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -1,2 +1,5 @@
 class DynamicArray:
-    def __init__(self, )
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.count = 0
+        self.storage = [None] * self.capacity
